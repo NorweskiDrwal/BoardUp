0cd01a59722255a06aa7d872435d9ace918d3a45
NorweskiDrwal <mateuszmsas@gmail.com>
2018-01-03 00:47:24 +0100

    added register page and firebase config

--- src/components/user/Register.vue
+++ src/components/user/Register.vue
@@ -1,7 +1,49 @@
 <template>
-  <div>
-    <p>The Register Page</p>
-  </div>
+  <v-container>
+    <v-layout row>
+      <v-flex xs12 sm6 offset-sm3>
+        <v-card>
+          <v-card-text>
+            <v-container>
+              <form>
+              <!-- email -->
+                <v-layout row>
+                  <v-flex xs12>
+                    <v-text-field name="email" label="Mail" id="email" v-model="email" type="email" required></v-text-field>  
+                  </v-flex>
+                </v-layout>
+              <!-- end email -->
+
+              <!-- password -->
+                <v-layout row>
+                  <v-flex xs12>
+                    <v-text-field name="password" label="Password" id="password" v-model="password" type="password" required></v-text-field>  
+                  </v-flex>
+                </v-layout>
+              <!-- end password -->
+
+              <!-- confirm password -->
+                <v-layout row>
+                  <v-flex xs12>
+                    <v-text-field name="confirmPassword" label="Confirm Password" id="confirmPassword" v-model="confirmPassword" type="password" :rules="[comparePasswords]"></v-text-field>  
+                  </v-flex>
+                </v-layout>
+              <!-- end confirm password -->
+
+              <!-- buttons -->
+                <v-layout row>
+                  <v-flex xs12>
+                    <v-btn type="submit">Register</v-btn> 
+                  </v-flex>
+                </v-layout>
+              <!-- end buttons -->
+              </form>
+            </v-container>
+          </v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
@@ -9,6 +51,20 @@ export default {
   name: 'Register',
   data () {
     return {
+      email: '',
+      password: '',
+      confirmPassword: ''
+    }
+  },
+  computed: {
+    comparePasswords () {
+      return this.password !== this.confirmPassword ? 'Passwords do not match' : ''
+    }
+  },
+  methods: {
+    onRegister () {
+      // Vuex
+      console.log({email: this.email, password: this.password, confirmPassword: this.confirmPassword})
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.css'
 
 import App from './App'
+import * as firebase from 'firebase'
 import router from './router'
 import { store } from './store'
 import DateFilter from './filters/date'
@@ -18,5 +19,14 @@ new Vue({
   el: '#app',
   router,
   store,
-  render: h => h(App)
+  render: h => h(App),
+  created () {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyAxvTGqZk71FQHDC_Gj-ahMWC1gC8VOmyM',
+      authDomain: 'boardup-24a7a.firebaseapp.com',
+      databaseURL: 'https://boardup-24a7a.firebaseio.com',
+      projectId: 'boardup-24a7a',
+      storageBucket: 'boardup-24a7a.appspot.com'
+    })
+  }
 })
